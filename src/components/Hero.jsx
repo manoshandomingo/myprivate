3db8646fbefe458215678efe33b6e5edafd8b210
Manoshan Domingo <manoshandomingo@mac.com>
2025-09-08 07:57:28 +1000

    updated the Hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div className='bg-teal-200 border h-[70vh]'>Hero</div>
+    <div className='bg-teal-200 border h-[70vh] p-8 flex flex-col'>
+      <div>
+        <img className='mx-auto w-[40vw] md:w-[40vw] h-auto' src='/images/ManoshanDom0.png'></img>  
+      </div>
+      <div><h1>Text area</h1></div>
+    </div>
   )
 }
 
